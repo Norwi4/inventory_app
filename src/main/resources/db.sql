0b8f6f622aae31454784f4df7e2809eda891fa6e
Абаев Евгений <abaev@flydrone.ru>
2023-12-18 22:15:33 +0300

    sql

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,9 +1,30 @@
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `wharehouse`;
+DROP TABLE IF EXISTS `provider`;
 DROP TABLE IF EXISTS `product`;
 DROP TABLE IF EXISTS `consignment`;
 DROP TABLE IF EXISTS `dish`;
 DROP TABLE IF EXISTS `dish_product`;
+
+CREATE TABLE `wharehouse`
+(
+    `id`   bigint(19) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE `provider`
+(
+    `id`               bigint(19) unsigned NOT NULL AUTO_INCREMENT,
+    `organizationName` varchar(255),
+    `responsible`      varchar(255),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `product`
 (
@@ -42,10 +63,10 @@ CREATE TABLE `dish`
 
 CREATE TABLE `dish_product`
 (
-    `id`      bigint(19) unsigned NOT NULL AUTO_INCREMENT,
-    `article` varchar(255),
+    `id`       bigint(19) unsigned NOT NULL AUTO_INCREMENT,
+    `article`  varchar(255),
     `quantity` bigint(19),
-    `dish_id` bigint(19),
+    `dish_id`  bigint(19),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
